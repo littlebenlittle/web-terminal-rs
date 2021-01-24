12c4c500b3efccb5baf8f499fd737fa1e10d60ec
Ben Little <ben.little@benlittle.dev>
2021-01-24 21:59:00 +0000

    Create timeout tests

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,53 +1,76 @@
-use std::error::Error;
 use tokio;
 use tokio::sync::{mpsc, oneshot};
+use tokio::time::{sleep, Duration};
 use tokio::task::JoinHandle;
-use web_terminal::run_server;
+use thiserror::Error;
 
 #[test]
-fn simple_e2e_test() -> Result<(), Box<dyn Error>> {
+fn e2e_test_timeout() -> Result<(), Box<dyn std::error::Error>> {
     let rt = tokio::runtime::Runtime::new()?;
-    rt.block_on( async { e2e_test().await })?;
-    assert!(false, "force test failure");
+    let timed_out = rt.block_on(timeout_harness(
+        Duration::from_millis(100),
+        Duration::from_millis(50),
+    ))?;
+    assert!(timed_out, "test should time out");
     Ok(())
 }
 
-async fn e2e_test() -> Result<(), String> {
-    let (server_stop_tx, server_stop_rx) = oneshot::channel::<()>();
-    let (server_msg_tx, mut server_msg_rx) = mpsc::channel(1);
+#[test]
+fn e2e_test_no_timeout() -> Result<(), Box<dyn std::error::Error>> {
+    let rt = tokio::runtime::Runtime::new()?;
+    let timed_out = rt.block_on(timeout_harness(
+        Duration::from_millis(50),
+        Duration::from_millis(100),
+    ))?;
+    assert!(timed_out == false, "test should not time out");
+    Ok(())
+}
 
-    let server = tokio::spawn(run_server(server_stop_rx, server_msg_tx));
+/// Ok(true) if test times out, Ok(false) if not
+async fn timeout_harness(
+    pause_duration:   Duration,  // time before task sends message
+    timeout_duration: Duration,  // time before test times out
+) -> Result<bool, String> {
+    let (stop_tx, stop_rx) = oneshot::channel::<()>();
+    let (msg_tx, mut msg_rx) = mpsc::channel(1);
 
-    let mut server_done = false;
-    let sleep = tokio::time::sleep(tokio::time::Duration::from_millis(500));
+    let task = tokio::spawn(timeout_test_task(
+        stop_rx,
+        msg_tx,
+        pause_duration,
+    ));
+
+    let mut task_done = false;
+    let mut timed_out = false;
+    let sleep = tokio::time::sleep(timeout_duration);
     tokio::pin!(sleep);
-    tokio::pin!(server);
-    while !server_done {
+    tokio::pin!(task);
+    while !task_done {
         tokio::select! {
             _ = &mut sleep => {
                 println!("test timed out");
+                timed_out = true;
                 break
             }
-            Some(msg) = server_msg_rx.recv() => {
-                println!("server: {}", msg);
-                server_done = true;
+            Some(msg) = msg_rx.recv() => {
+                println!("task: {}", msg);
+                task_done = true;
             }
         }
     }
 
-    ensure_task_stopped("server", server, server_stop_tx).await?;
+    ensure_task_stopped("task", task, stop_tx).await?;
     
-    Ok(())
+    Ok(timed_out)
 }
 
 async fn ensure_task_stopped<T,E>(
     task_name: &str,
     task_jh: std::pin::Pin<&mut JoinHandle<Result<T,E>>>,
     stop_tx: oneshot::Sender<()>,
-)
--> Result<(), String>
+) -> Result<(), String>
 where
-    E: std::fmt::Display + Error
+    E: std::fmt::Display + std::error::Error
 {
     if !stop_tx.is_closed() {
         if let Err(_) = stop_tx.send(()) {
@@ -63,4 +86,34 @@ where
         }
     }
     Ok(())
+}
+
+async fn timeout_test_task(
+    stop_rx: oneshot::Receiver<()>,
+    msg_tx:  mpsc::Sender<String>,
+    pause_duration: Duration,
+)
+-> Result<(), Error>
+{
+    let timeout = sleep(pause_duration);
+    tokio::pin!(timeout);
+    tokio::select! {
+        _ = &mut timeout => { }
+        result = stop_rx => {
+            result?;
+            println!("task received stop signal");
+            return Ok(())
+        }
+    }
+    msg_tx.send("done".to_string()).await?;
+    println!("task sent message \"done\"");
+    Ok(())
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("tokio mpsc send string error")]
+    TokioMpscSendString(#[from] mpsc::error::SendError<std::string::String>),
+    #[error("tokio oneshot recv error")]
+    TokioOneshotRecv(#[from] tokio::sync::oneshot::error::RecvError),
 }
