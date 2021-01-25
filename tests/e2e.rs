ffe47c5adcb07e4bfaa71c2ef219ae8aea9b279a
Ben Little <ben.little@benlittle.dev>
2021-01-25 17:10:37 +0000

    Get basic tests running

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,8 +1,13 @@
+use std::net::SocketAddr;
 use tokio;
 use tokio::sync::{mpsc, oneshot};
 use tokio::time::{sleep, Duration};
 use tokio::task::JoinHandle;
+use async_tungstenite::tungstenite::Message;
+use async_tungstenite::tokio as tokio_tungstenite;
 use thiserror::Error;
+use futures_util::{SinkExt, StreamExt};
+use url::Url;
 
 #[test]
 fn e2e_test_timeout() -> Result<(), Box<dyn std::error::Error>> {
@@ -27,10 +32,14 @@ fn e2e_test_no_timeout() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn test_websocket_connect() -> Result<(), Box<dyn std::error::Error>> {
+fn test_websocket_connection() -> Result<(), Box<dyn std::error::Error>> {
     let rt = tokio::runtime::Runtime::new()?;
+    let port = random_port();
+    let addr = format!("127.0.0.1:{}", port);
     let (client_connected, server_connected) = rt.block_on(websocket_connect_harness(
-        Duration::from_millis(1000),
+        Duration::from_millis(100),
+        format!("ws://{}", addr).parse().unwrap(),
+        addr.parse()?,
     ))?;
     assert!(client_connected, "connection should have been established");
     assert!(server_connected, "connection should have been established");
@@ -38,10 +47,20 @@ fn test_websocket_connect() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn test_websocket_connect_failed() -> Result<(), Box<dyn std::error::Error>> {
+fn test_failed_websocket_connection() -> Result<(), Box<dyn std::error::Error>> {
     let rt = tokio::runtime::Runtime::new()?;
+    let mut port;
+    loop {
+        port = random_port();
+        if port != 12345 {
+            break
+        }
+    }
+    let addr = format!("127.0.0.1:{}", port).parse()?;
     let (client_connected, server_connected) = rt.block_on(websocket_connect_harness(
-        Duration::from_millis(1000),
+        Duration::from_millis(250),
+        "ws://127.0.0.1:12345".parse().unwrap(),
+        addr,
     ))?;
     assert!(!client_connected, "connection should not have been established");
     assert!(!server_connected, "connection should not have been established");
@@ -94,18 +113,18 @@ async fn ensure_task_stopped<T,E>(
 where
     E: std::fmt::Display + std::error::Error
 {
+    println!("stopping task {}", task_name);
     if !stop_tx.is_closed() {
         if let Err(_) = stop_tx.send(()) {
             return Err(format!("failed to send {} stop signal", task_name))
-        } else {
-            match task_jh.await {
-                Ok(v) => match v {
-                    Ok(_)  => println!("{} exited OK", task_name),
-                    Err(e) => println!("{} exited Err: {}", task_name, e)
-                }
-                Err(e) => return Err(format!("failed to join with {} task: {}", task_name, e))
-            }
         }
+    }
+    match task_jh.await {
+        Ok(v) => match v {
+            Ok(_)  => println!("{} exited OK", task_name),
+            Err(e) => println!("{} exited Err: {}", task_name, e)
+        }
+        Err(e) => return Err(format!("failed to join with {} task: {}", task_name, e))
     }
     Ok(())
 }
@@ -135,41 +154,48 @@ async fn timeout_test_task(
 pub enum Error {
     #[error("tokio mpsc send string error")]
     TokioMpscSendString(#[from] mpsc::error::SendError<std::string::String>),
+    #[error("tokio mpsc send string error")]
+    TokioMpscSendSocketAddr(#[from] mpsc::error::SendError<std::net::SocketAddr>),
     #[error("tokio oneshot recv error")]
-    TokioOneshotRecv(#[from] tokio::sync::oneshot::error::RecvError),
+    TokioOneshotRecv(#[from] oneshot::error::RecvError),
+    #[error("failed to establish websocket connection")]
+    WebSocketConnect(#[from] async_tungstenite::tungstenite::Error),
+    #[error("failed to join a tokio task's join handle")]
+    TokioTaskJoinError(#[from] tokio::task::JoinError),
 }
 
 async fn websocket_connect_harness(
     timeout_duration: Duration,  // time before test times out
+    client_addr: Url,
+    server_addr: SocketAddr,
 ) -> Result<(bool, bool), String> {
-    let (client_stop_tx, client_stop_rx) = oneshot::channel::<()>();
-    let (client_connect_tx, mut client_connect_rx) = mpsc::channel::<String>(1);
-    let (client_msg_tx, mut client_msg_rx) = mpsc::channel(1);
+    let (client_connect_tx, mut client_connect_rx) = mpsc::channel::<Url>(1);
+    let (client_msg_tx, _) = mpsc::channel(1);
     let client = tokio::spawn(run_websocket_client(
-        client_stop_rx,
+        client_addr,
         client_connect_tx,
         client_msg_tx,
+        Duration::from_secs(2),
     ));
 
-    let (server_stop_tx, server_stop_rx) = oneshot::channel::<()>();
-    let (server_connect_tx, mut server_connect_rx) = mpsc::channel::<String>(1);
-    let (server_msg_tx, mut server_msg_rx) = mpsc::channel(1);
+    let (server_connect_tx, mut server_connect_rx) = mpsc::channel::<SocketAddr>(1);
+    let (server_msg_tx, _) = mpsc::channel(1);
     let server = tokio::spawn(run_websocket_server(
-        server_stop_rx,
+        server_addr,
         server_connect_tx,
         server_msg_tx,
     ));
 
-    let (mut client_done, mut server_done) = (false, false);
     let (mut client_connected, mut server_connected) = (false, false);
     let sleep = tokio::time::sleep(timeout_duration);
     tokio::pin!(sleep);
     tokio::pin!(client);
     tokio::pin!(server);
-    while !(client_done && server_done) {
+    while !(client_connected && server_connected) {
         tokio::select! {
             _ = &mut sleep => {
-                panic!("test timed out");
+                println!("test timed out");
+                break
             }
             Some(addr) = client_connect_rx.recv() => {
                 println!("client connected: {}", addr);
@@ -179,35 +205,145 @@ async fn websocket_connect_harness(
                 println!("server connected: {}", addr);
                 server_connected = true;
             }
-            Some(msg) = client_msg_rx.recv() => {
-                println!("client received: {}", msg);
-                client_done = true;
-            }
-            Some(msg) = server_msg_rx.recv() => {
-                println!("server received: {}", msg);
-                server_done = true;
-            }
         }
     }
 
-    ensure_task_stopped("client", client, client_stop_tx).await?;
-    ensure_task_stopped("server", server, server_stop_tx).await?;
-    
     Ok((client_connected, server_connected))
 }
 
 async fn run_websocket_client(
-    stop_rx: oneshot::Receiver<()>,
-    connect_tx: mpsc::Sender<String>,
+    addr: Url,
+    connect_tx: mpsc::Sender<Url>,
     msg_tx: mpsc::Sender<String>,
+    give_up_after: Duration,
 ) -> Result<(), Error> {
+    println!("client dialing {}", addr);
+    let stream;
+    let timeout = sleep(give_up_after);
+    tokio::pin!(timeout);
+    loop {
+        tokio::select! {
+            _ = &mut timeout => {
+                println!("timed out waiting for connection after {:?}", give_up_after);
+            }
+            result = tokio_tungstenite::connect_async(addr.clone()) => {
+                match result {
+                    Ok((s, _)) => {
+                        stream = s;
+                        break
+                    }
+                    Err(e) => return Err(e.into())
+                }
+            }
+        }
+        sleep(Duration::from_millis(200)).await
+    }
+    
+    let (mut tx, mut rx) = stream.split();
+    tx.send(Message::text("test message".to_owned()))
+        .await
+        .expect("client failed to send test message");
+    connect_tx.send(addr.clone())
+        .await
+        .expect("client failed to send msg");
+    loop {
+        match rx.next().await {
+            Some(msg) => {
+                match msg.expect("client could not load message") {
+                    Message::Text(s) => {
+                        if msg_tx.is_closed() {
+                            println!("client channel to main thread is already closed -- that may be OK");
+                            break
+                        }
+                        match msg_tx.send(s).await {
+                            Ok(_) => println!("client passed msg to main thread"),
+                            Err(e) => panic!("client failed to pass msg to main thread: {}", e)
+                        }
+                    }
+                    Message::Close(f) => {
+                        let mut m = "".to_string();
+                        match f {
+                            Some(v) => { m = format!(": {} {}", v.code, v.reason) }
+                            None => {}
+                        }
+                        println!("connection from {} closed{}", addr, m);
+                        break
+                    }
+                    _ => { println!("non-text message") }
+                }
+            }
+            None => {
+                println!("no message retrived from stream");
+                break
+            }
+        }
+    }
     Ok(())
 }
 
 async fn run_websocket_server(
-    stop_rx: oneshot::Receiver<()>,
-    connect_tx: mpsc::Sender<String>,
+    addr: SocketAddr,
+    connect_tx: mpsc::Sender<SocketAddr>,
     msg_tx: mpsc::Sender<String>,
 ) -> Result<(), Error> {
+    println!("server listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .expect("server failed to bind");
+    let (stream, _) = listener.accept()
+        .await
+        .expect("server failed to accept connection");
+    let peer = stream
+        .peer_addr()
+        .expect("server: connected streams should have a peer address");
+    println!("server: new connection from {}", peer);
+    connect_tx.send(peer).await.unwrap();
+    let websocket = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("server: error during websocket handshake");
+
+    let (mut tx, mut rx) = websocket.split();
+    tx.send(Message::text("test message".to_owned()))
+        .await
+        .expect("server failed to send message");
+    loop {
+        match rx.next().await {
+            Some(msg) => {
+                match msg.expect("could not load message") {
+                    Message::Text(s) => {
+                        if msg_tx.is_closed() {
+                            println!("server channel to main thread is already closed -- that may be OK");
+                            break
+                        }
+                        match msg_tx.send(s).await {
+                            Ok(_) => println!("server passed msg to main thread"),
+                            Err(e) => panic!("server failed to pass msg to main thread: {}", e)
+                        }
+                    }
+                    Message::Close(f) => {
+                        let mut m = "".to_string();
+                        match f {
+                            Some(v) => { m = format!(": {} {}", v.code, v.reason) }
+                            None => {}
+                        }
+                        println!("connection from {} closed{}", peer, m);
+                        break
+                    }
+                    _ => { println!("non-text message") }
+                }
+            }
+            None => {
+                println!("no message retrived from stream");
+                break
+            }
+        }
+    }
     Ok(())
+}
+
+fn random_port() -> u16 {
+    let addr = "127.0.0.1:0".parse::<std::net::SocketAddr>().unwrap();
+    let sock = std::net::UdpSocket::bind(addr).unwrap();
+    let local_addr = sock.local_addr().unwrap();
+    local_addr.port()
 }
